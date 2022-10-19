2f1aacb8aadec95f65665bb0735740d5323aec51
yebepoga <ypomag14@gmail.com>
2022-10-18 22:29:18 -0500

    Pokebola

--- Semana2_1_2/Semana2_1_2.cpp
+++ Semana2_1_2/Semana2_1_2.cpp
@@ -1,240 +1,195 @@
 #include<GL/glut.h>
 #include <math.h>
 
-void cielo() {
+void fondo() {
+	glBegin(GL_QUADS);
+	glColor3ub(230,231,232);
+	glVertex2d(0, 9);
+	glVertex2d(14, 9);
+
+	glColor3ub(109, 110, 111);
+	glVertex2d(14, 4.5);
+	glVertex2d(0, 4.5);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(109, 110, 111);
+	glVertex2d(14,4.5);
+	glVertex2d(0,4.5);
+	glColor3ub(230, 231, 232);
+	glVertex2d(0,0);
+	glVertex2d(14,0);
+	glEnd();
+
+}
+void circuloNegro() {
 	glBegin(GL_POLYGON);
-	glColor3ub(253, 41, 7);
-	glVertex2d(0, 12);
-	glVertex2d(18, 12);
-	glColor3ub(248, 202, 155);
-	glVertex2d(18, 4);
-	glVertex2d(0, 4);
+	float radio = 3.5;
+	float cx, cy;
+	//glColor4ub(0, 0, 0,20); //PARA TRASPARENCIA
+	glColor3ub(0, 0, 0);
+	for (double i = 0; i < 3.1416 * 2; i += 0.001) {
+		cx = radio * cos(i);
+		cy = radio * sin(i);
+		glVertex2d(cx + 7, cy + 4.5);
+		}
+	glEnd();
+	}
+
+void circuloRojo() {
+
+	glBegin(GL_POLYGON);
+	float radio = 3.6;
+	float cx, cy;
+	//Para hacer degradado
+	float colorInicial1 = 237;
+	float colorInicial2 = 27;
+	float colorInicial3 = 36;
+
+	float colorFinal1 = 140;
+	float colorFinal2 = 12;
+	float colorFinal3 = 17;
+
+	for (double r = 0; r <= radio; r += 0.1) {
+		glColor3ub(
+			(colorInicial1 - ((colorInicial1 - colorFinal1) / radio * r)),
+			(colorInicial2 - ((colorInicial2 - colorFinal2) / radio * r)),
+			(colorInicial3 - ((colorInicial3 - colorFinal3) / radio * r))
+			);
+		
+	for (double i = 0; i < 3.1416 ; i += 0.001) { // No se multiplica *2 para solo mostrar la mitad
+		cx = r * cos(i);
+		cy = r * sin(i);
+		glVertex2d(cx + 7, cy + 4.7);
+		}
+	}
 	glEnd();
 }
-void estrellas() {
-	glPointSize(5);
 
+void circuloBlanco() {
+
+	glBegin(GL_POLYGON);
+	float radio = 3.6;
+	float cx, cy;
+
+	float colorInicial1 = 255;
+	float colorInicial2 = 255;
+	float colorInicial3 = 255;
+
+	float colorFinal1 = 154;
+	float colorFinal2 = 154;
+	float colorFinal3 = 154;
+
+	for (double r = 0; r <= radio; r += 0.1) {
+		glColor3ub(
+			(colorInicial1 - ((colorInicial1 - colorFinal1) / radio * r)),
+			(colorInicial2 - ((colorInicial2 - colorFinal2) / radio * r)),
+			(colorInicial3 - ((colorInicial3 - colorFinal3) / radio * r))
+		);
+
+			for (double i = 3.1416; i < 3.1416 * 2; i += 0.001) {
+			cx = r * cos(i);
+			cy = r * sin(i);
+			glVertex2d(cx + 7, cy + 4.3);
+		}
+	}
+	glEnd();
+}
+void circulo1(){
+	glBegin(GL_POLYGON);
+		float radio = 1.5;
+		float cx, cy;
+	glColor3ub(0, 0, 0);
+	for (double i = 0; i < 3.1416 * 2; i += 0.001) {
+		cx = radio * cos(i);
+		cy = radio * sin(i);
+	glVertex2d(cx+7,  cy +4.5);
+	}
+
+glEnd();
+
+}
+void circulo2() {
+	glBegin(GL_POLYGON);
+	float radio = 1.5;
+	float cx, cy;
+	glColor3ub(0, 0, 0);
+	for (double i = 0; i < 3.1416 * 2; i += 0.001) {
+		cx = radio * cos(i);
+		cy = radio * sin(i);
+		glVertex2d(cx + 7, cy + 4.5);
+	}
+
+	glEnd();
+
+}
+void circulo3() {
+	glPointSize(4);
 	glBegin(GL_POINTS);
-	glColor3ub(255, 218, 193);
-	glVertex2d(3.27, 6.66);
-	glVertex2d(4.92, 6.12);
-	glVertex2d(6.57, 8.92);
-	glVertex2d(10.86, 9.99);
-	glVertex2d(10.88, 7.22);
-	glVertex2d(12.51, 11.72);
-	glVertex2d(13.64, 10.04);
-	glVertex2d(16.33, 8.95);
-	glVertex2d(15.25, 6.14);
-
-
-	glEnd();
-}
-void mar() {
-	glBegin(GL_QUADS);
-	glColor3ub(248, 43, 11);
-	glVertex2d(0, 4);
-	glVertex2d(18, 4);
-	glColor3ub(15, 166, 225);
-	glVertex2d(18, 0);
-	glVertex2d(0, 0);
-
-
-	glEnd();
-}
-void luna1() {
-	float radio = 3.16;
+	float radio = 0.8;
 	float cx, cy;
-	glColor3ub(250, 218, 186);
-
-	glBegin(GL_POLYGON);
+	glColor3ub(105,105,107);
 	for (double i = 0; i < 3.1416 * 2; i += 0.001) {
 		cx = radio * cos(i);
 		cy = radio * sin(i);
-
-		glVertex2d(cx + 2, cy + 11);
+		glVertex2d(cx + 7, cy + 4.5);
 	}
+
 	glEnd();
 
-
 }
-void luna2() {
-	float radio = 3.16;
+void circuloSombra() {
+
+	glBegin(GL_POLYGON);
+	float radio = 3.6;
 	float cx, cy;
-	glColor3ub(218, 94, 84);
 
-	glBegin(GL_POLYGON);
-	for (double i = 0; i < 3.1416 * 2; i += 0.001) {
-		cx = radio * cos(i);
-		cy = radio * sin(i);
+	float colorInicial1 = 0;
+	float colorInicial2 = 0;
+	float colorInicial3 = 0;
 
-		glVertex2d(cx + 1.9, cy + 11.2);
+	float colorFinal1 = 194;
+	float colorFinal2 = 195;
+	float colorFinal3 = 196;
+
+	for (double r = 0; r <= radio; r += 0.01) {
+		glColor4ub(
+			(colorInicial1 - ((colorInicial1 - colorFinal1) / radio * r)),
+			(colorInicial2 - ((colorInicial2 - colorFinal2) / radio * r)),
+			(colorInicial3 - ((colorInicial3 - colorFinal3) / radio * r)),
+			(255-(255/radio*r))
+		);
+
+		//glColor3ub(237,27,36);
+		for (double i = 0; i < 3.1416*2; i += 0.001) {
+			cx = r * cos(i);
+			cy = r * sin(i);
+			glVertex2d(cx + 7, cy + 0.2);
+		}
 	}
-	glEnd();
-}
-void base() {
-	glColor3ub(15, 19, 19);
-	glBegin(GL_POLYGON);
-	glVertex2d(7.1, 2.75);
-	glVertex2d(12.31, 2.75);
-	glVertex2d(12, 2.2);
-	glVertex2d(7.42, 2.2);
-
-	glEnd();
-
-}
-void mastil() {
-	glColor3ub(15, 19, 19);
-	glBegin(GL_QUADS);
-	glVertex2d(8.7, 5.8);
-	glVertex2d(8.9, 5.8);
-	glVertex2d(8.9, 2.5);
-	glVertex2d(8.7, 2.5);
-
-	glEnd();
-}
-void vela() {
-	glColor3ub(242, 156, 139);
-	glBegin(GL_POLYGON);
-	glVertex2d(8.83, 5.53);
-	glVertex2d(9.82, 3.33);
-	glVertex2d(8.79, 2.72);
-	glVertex2d(7.98, 3.33);
-
-	glEnd();
-
-}
-void velero() {
-	glPushMatrix();
-
-	base();
-	vela();
-	mastil();
-	glPopMatrix();
-}
-void basefaro() {
-	float radio = 2.14;
-	float cx, cy;
-	glColor3ub(84, 175, 54);
-	glBegin(GL_POLYGON);
-	for (double i = 0; i < 3.1416 * 2; i += 0.01) {
-		cx = radio * cos(i);
-		cy = radio * sin(i);
-
-		glVertex2d(cx + 15.5, cy - 1);
-	}
-	glEnd();
-}
-void faro() {
-	glColor3ub(255, 255, 255);
-	glBegin(GL_QUADS);
-	glVertex2d(14.8, 0.89);
-	glVertex2d(15, 4.5);
-	glVertex2d(16, 4.5);
-	glVertex2d(16.4, 0.89);
-	glEnd();
-
-	glColor3ub(255, 0, 0);
-	glBegin(GL_POLYGON);
-	glVertex2d(14.88, 2.38);
-	glVertex2d(16.22, 2.54);
-	glVertex2d(16.29, 1.91);
-	glVertex2d(14.85, 1.76);
-	glEnd();
-
-	glColor3ub(255, 0, 0);
-	glBegin(GL_POLYGON);
-	glVertex2d(14.96, 3.76);
-	glVertex2d(16.07, 3.87);
-	glVertex2d(16.14, 3.25);
-	glVertex2d(14.92, 3.12);
-	glEnd();
-
-	glColor3ub(15, 19, 16);
-	glBegin(GL_POLYGON);
-	glVertex2d(14.72, 4.8);
-	glVertex2d(16.31, 4.81);
-	glVertex2d(16, 4.5);
-	glVertex2d(15, 4.5);
-	glEnd();
-
-	glColor3ub(255, 242, 0);
-	glBegin(GL_QUADS);
-	glVertex2d(14.99, 5.7);
-	glVertex2d(16.01, 5.7);
-	glVertex2d(16, 4.8);
-	glVertex2d(15, 4.8);
-	glEnd();
-
-	glColor3ub(15, 19, 19);
-	glBegin(GL_LINES);
-	glVertex2d(15.5, 5.7);
-	glVertex2d(15.5, 4.81);
-	glEnd();
-
-	glColor3ub(15, 19, 19);
-	glBegin(GL_LINES);
-	glVertex2d(15.2, 5.7);
-	glVertex2d(15.2, 4.8);
-	glEnd();
-
-	glColor3ub(15, 19, 19);
-	glBegin(GL_LINES);
-	glVertex2d(15.8, 5.7);
-	glVertex2d(15.8, 4.81);
-	glEnd();
-
-	glColor3ub(163, 73, 164);
-	glBegin(GL_POLYGON);
-	glVertex2d(14.5, 6.09);
-	glVertex2d(15.5, 6.6);
-	glVertex2d(16.51, 6.12);
-	glVertex2d(16.51, 5.69);
-	glVertex2d(14.49, 5.69);
-	glEnd();
-
-	glColor3ub(15, 19, 19);
-	glBegin(GL_LINES);
-	glVertex2d(15.5, 7.5);
-	glVertex2d(15.5, 6.59);
-	glEnd();
-
-	glColor3ub(255, 255, 0);
-	glBegin(GL_TRIANGLES);
-	glVertex2d(15.5, 7.5);
-	glVertex2d(16.28, 7.32);
-	glVertex2d(15.5, 7);
 	glEnd();
 }
 
 void dibujar() {
 	glLoadIdentity();
-	gluOrtho2D(0, 18, 0, 12);
+	gluOrtho2D(0, 14, 0, 9);
 	glClear(GL_COLOR_BUFFER_BIT);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //Para la trasparencia 
+	glEnable(GL_BLEND);//trasparencia
 
-	cielo();
-	estrellas();
-	mar();
-	luna1();
-	luna2();
-	velero();
-	basefaro();
-	faro();
-
-	/*for (int i = -7; i < 5; i += 2)
-	{
-		glPushMatrix();
-			glTranslated(i, 0, 0);
-			velero();
-		glPopMatrix();
-
-	}*/
+	fondo();
 	glPushMatrix();
-	glTranslated(-2, 0, 0);
-	glScaled(0.6, 0.6, 0.6);
-	velero();
+	glTranslated(-3.5, 0, 0);
+	glScaled(1.5, 0.7, 1);
+	circuloSombra();
 	glPopMatrix();
-
-
+	circuloNegro();
+	circuloRojo();
+	circuloBlanco();
+	circulo1();
+	circulo2();
+	circulo3();
+	
 
 	glFlush();
 }
